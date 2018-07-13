6cae514d14bbd0e6f38276b73a69be2616c55e88
Tomasz Kot <tomek@imac-tomasz.home>
2018-07-13 18:02:36 +0200

    Improvements

--- app/assets/javascripts/animation.js
+++ app/assets/javascripts/animation.js
@@ -1,14 +1,11 @@
 $(function() {
+    $('.new-author').on('ajax:send', function(){
+        $('.new-author').append('<img class="loader" src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif"/>');
+    });
 
-$('h1').css({color: 'red'});
-
-$('.new-author').on('ajax:send', function(){
-    $('.new-author').append('<img class="loader" src="https://media.giphy.com/media/N256GFy1u6M6Y/giphy.gif"/>');
-});
-
-$('.new-author').on('ajax:complete', function(){
-    $('.loader').remove();    
-});
+    $('.new-author').on('ajax:complete', function(){
+        $('.loader').remove();    
+    });
 
 } );
 // ta funkcja jest zeby opoznic 
